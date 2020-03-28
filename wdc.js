262991378d8fb04d2ca029cc5ece5dbb5c1c8c03
Nate Silverblatt <nsilverblatt@gmail.com>
2020-03-28 14:39:56 -0400

    Update wdc.js

--- wdc.js
+++ wdc.js
@@ -1,57 +1,112 @@
-(function () {
-var myConnector = tableau.makeConnector();
-myConnector.getSchema = function (schemaCallback) {
-var cols = [
-{ id : "date", alias:"Date", dataType : tableau.dataTypeEnum.string },
-{ id : "trips", alias: "Trips Per Day",dataType : tableau.dataTypeEnum.string },
-{ id : "farebox",alias: "Farebox Per Day", dataType : tableau.dataTypeEnum.string },
-{ id : "uniquemed",alias: "Unique Medallions", dataType : tableau.dataTypeEnum.string },
-{ id : "uniquedrivers", alias: "Unique Drivers", dataType : tableau.dataTypeEnum.string},
-{ id : "medperday", alias: "Medallions Per Day", dataType : tableau.dataTypeEnum.string },
-{ id : "avg1", alias: "Average Days Medallions On Road",  dataType : tableau.dataTypeEnum.string },
-{ id : "avg2", alias: "Avg Hours Per Day Per Medallion",  dataType : tableau.dataTypeEnum.string},
-{ id : "avg3", alias:"Avg Days Drivers on Road",  dataType : tableau.dataTypeEnum.string},
-{ id : "avg4", alias:"Avg Hours Per Day Per Driver", dataType : tableau.dataTypeEnum.string},
-{ id : "avg5", alias: "Avg Minutes Per Trip",  dataType : tableau.dataTypeEnum.string},
-{ id : "cc", alias: "Percent of Trips Paid with Credit Card", dataType : tableau.dataTypeEnum.string}
-];
-var tableInfo = {
-id : "taxi",
-alias : "TLC Trip Data",
-columns : cols
-};
-schemaCallback([tableInfo]);
-};
-myConnector.getData = function(table, doneCallback) {
-$.getJSON("http://www.nyc.gov/html/tlc/downloads/csv/data_reports_monthly_indicators_yellow.json", function(resp) {
-	var feat = resp;
-	tableData = [];
-	// Iterate over the JSON object
-	for (var i = 0, len = feat.length; i < len; i++) {
-	tableData.push({
-	"date": feat[i]["Month"]["Year"],
-	"trips": feat[i]["Trips Per Day"],
-	"farebox": feat[i] ["Farebox Per Day"],
-	"uniquemed": feat[i] ["Unique Medallions"],
-	"uniquedrivers": feat[i] ["Unique Drivers"],
-	"medperday": feat[i] ["Medallions Per Day"],
-	"avg1": feat[i] ["Avg Days Medallions on Road"],
-	"avg2": feat[i] ["Avg Hours Per Day Per Medallion"] ,
-	"avg3": feat[i] ["Avg Days Drivers on Road"],
-	"avg4": feat[i] ["Avg Hours Per Day Per Driver"],
-	"avg5": feat[i] ["Avg Minutes Per Trip"],
-	"cc": feat[i] ["Percent of Trips Paid with Credit Card"]
+$(document).ready(function (datasource) {
+
+	var indicator = $('#CSV').val();
+	var datasource = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv";
+	var myConnector = tableau.makeConnector();
+	
+	$('#CSV').on('change keyup paste click', function() {
+    indicator = $('#CSV').val();
+	datasource = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv";
+	tableau.connectionData = datasource;
 	});
-	}
-	table.appendRows(tableData);
-	doneCallback();
+	
+	myConnector.getSchema = function (schemaCallback) {
+
+		var source = tableau.connectionData;
+		
+		$.ajax({
+			url: source,
+			dataType: "text"
+		}).done(successFunction);
+
+		function successFunction(data) {
+			var data = data.replace(/\"/g, "");
+			var data = data.replace(/ /g, '');
+			var allRows = data.split(/\r?\n|\r/);
+			var cols = [];
+			for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+				var rowCells = allRows[singleRow].split(',');
+				for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+					if (singleRow === 0) {
+						x = rowCells[rowCell];
+
+						y = {
+							id: x,
+							alias: x,
+							dataType: tableau.dataTypeEnum.string
+						};
+						cols.push(y);
+					}
+				}
+			}
+			console.log(cols);
+			var tableInfo = {
+				id: "WDCcsv",
+				alias: "WDCcsv",
+				columns: cols
+			};
+
+			schemaCallback([tableInfo]);
+		}
+	};
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	myConnector.getData = function (table, doneCallback) {
+
+		
+		var source = tableau.connectionData;
+		
+		$.ajax({
+			url: source,
+			dataType: "text",
+		}).done(successFunction);
+
+		function successFunction(data) {
+			var data = data.replace(/\"/g, "");
+			var allRows = data.split(/\r?\n|\r/);
+			var tableData = [];
+			var cols = [];
+			for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+				var rowCells = allRows[singleRow].split(',');
+				for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+					if (singleRow === 0) {
+						x = rowCells[rowCell];
+						y = {
+							id: x,
+							alias: x,
+							dataType: tableau.dataTypeEnum.string
+						};
+						cols.push(y);
+					}
+				}
+				if (singleRow != 0) {
+					x = rowCells;
+
+					tableData.push(x);
+				}
+			}
+
+			table.appendRows(tableData);
+			doneCallback();
+		}
+	};
+
+	tableau.registerConnector(myConnector);
+
+	$(document).ready(function () {
+		$("#submitButton").click(function () {
+			indicator = $('#CSV').val();
+			datasource = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv";
+			tableau.connectionData = datasource;
+			tableau.connectionName = "WDCcsv";
+			tableau.connectionData = datasource;
+			tableau.submit();
+		});
 	});
 
-};
-tableau.registerConnector(myConnector);
-$(document).ready(function () {
-$("#submitButton").click(function () {
-tableau.connectionName = "taxi";
-tableau.submit();
+	
+
 });
-});})();
